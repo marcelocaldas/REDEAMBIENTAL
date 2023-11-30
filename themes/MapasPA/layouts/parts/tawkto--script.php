f6fc668fa1df886151632dfdb6c5ebeba0b440d3
marcelocaldas <38535161+marcelocaldas@users.noreply.github.com>
2023-11-30 14:17:22 -0300

    Update tawkto--script.php
    retirar o tawkto do pará

--- themes/MapasPA/layouts/parts/tawkto--script.php
+++ themes/MapasPA/layouts/parts/tawkto--script.php
@@ -6,7 +6,7 @@
         var s1 = document.createElement("script"),
             s0 = document.getElementsByTagName("script")[0];
         s1.async = true;
-        s1.src = 'https://embed.tawk.to/5f47c17bcc6a6a5947af53ba/default';
+        s1.src = 'https://colmeia.redeambiental.net';
         s1.charset = 'UTF-8';
         s1.setAttribute('crossorigin', '*');
         s0.parentNode.insertBefore(s1, s0);
